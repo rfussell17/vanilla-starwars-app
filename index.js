a04b56c23ae79049ddc9435ec9766d007edc31f1
Robin Fussell <rfussell17@gmail.com>
2020-12-17 20:04:07 -0800

    added error handling to api call

--- index.js
+++ index.js
@@ -10,23 +10,23 @@ btn.addEventListener("click", function () {
 async function getData() {
   for (let i = 1; i <= 10; i++) {
     const res = await fetch(`https://swapi.dev/api/people/${i}`);
-    if(res.ok){
+    if (res.ok) {
       const data = await res.json();
       const row = table.insertRow();
-  
+
       const birthNameData = row.insertCell();
       birthNameData.innerHTML = data.name;
-  
+
       const birthYearData = row.insertCell();
       birthYearData.innerHTML = data.birth_year;
-  
+
       const heightData = row.insertCell();
       heightData.innerHTML = data.height;
-  
+
       const massData = row.insertCell();
       massData.innerHTML = data.mass;
     } else {
-      console.error(`Error: ${res.status}`)
+      console.error(`Error: ${res.status}`);
     }
   }
   document.body.appendChild(table);
